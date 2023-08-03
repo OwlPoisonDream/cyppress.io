f4e008a459a5e481ee72fc55caf1388bd05def45
OwlPoisonDream <90618173+OwlPoisonDream@users.noreply.github.com>
2023-08-03 10:37:24 +0300

    Update spec.cy.js

--- test/cypress/e2e/spec.cy.js
+++ test/cypress/e2e/spec.cy.js
@@ -1,21 +1,21 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://forhemer.github.io/React-Todo-List/')
-    cy.get('[name="title"]').type("Первое дело")
+    cy.visit('https://forhemer.github.io/React-Todo-List/') //Переход на сайт
+    cy.get('[name="title"]').type("Первое дело") //Ввод первых данных
     cy.get('[type="submit"]').click()
-    cy.get('[name="title"]').type("Второе дело")
+    cy.get('[name="title"]').type("Второе дело") //Ввод вторых данных
     cy.get('[type="submit"]').click()
-    cy.get('[name="title"]').type("Третье дело")
+    cy.get('[name="title"]').type("Третье дело") //Ввод третьих данных
     cy.get('[type="submit"]').click()
-    cy.get('[class="TodoItem_item__iFWQW"]')
-    cy.get('[type="checkbox"]')
+    cy.get('[class="TodoItem_item__iFWQW"]') //Считаю элементы
+    cy.get('[type="checkbox"]') //Подтвреждаю первый чекбокс
       .first()
       .check()
-    cy.get('[class="TodoItem_item__iFWQW"]')
+    cy.get('[class="TodoItem_item__iFWQW"]') //Смотрю наличие перечеркнутого текста
       .get('li:first span')
       .should('have.css', 'text-decoration-line', 'line-through')
-    cy.get('[type="button"]:first').click()
-    cy.get('[class="TodoItem_item__iFWQW"]')
+    cy.get('[type="button"]:first').click() //Удаляю первый элемент
+    cy.get('[class="TodoItem_item__iFWQW"]') //Ищу данные удалённого элемента
       .get('li:first span')
       .contains('Первое дело')
   })
